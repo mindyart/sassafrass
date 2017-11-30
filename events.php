b2dfc7bebf12964632f027ac7e3a474bdfc76170
Sam <villaluzsam@gmail.com>
2017-11-29 19:34:58 -0500

    progress bar

--- events.php
+++ events.php
@@ -1,7 +1,9 @@
 
 <html lang="en">
   <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   </head>
   <body>
     <h1>Events</h1>
@@ -21,23 +23,24 @@
             <td>Secret Santa. This event will be held on December 4th, 2017. <br></td>
 			<?php
 			include "dbconnection.php";
-			$con = getdb(); 
+			$con = getdb();
 			$Sql = "SELECT e.Deadline
 					, e.Going
 					,e.Threshold
 					FROM Events e
-					WHERE e.EventID = 1;
+					WHERE e.EventID = 1"
+      ?>
 
-//<!-- Progress - pull going/threshold from database-->
-    
-//<!-- pull deadline -->
-            <td></td>
-			
+<!-- Progress - pull going/threshold from database-->
+
+ <!-- pull deadline -->
+            <td>
+            <div class="progress-wrap-sv progress-sv" data-progress-percent="40">
+              <div class="progress-bar-sv progress-sv"></div>
+            </div>
+            </td>
+
 			<td></td>
-			
-
-			
-			?>
 <!-- get michelle's google authentication variable and add to database + increment going by 1 -->
             <td></td>
           </tr>
@@ -50,5 +53,29 @@
           </tr>
         </tbody>
       </table>
+
+      <script>
+      // on page load...
+  moveProgressBar();
+  // on browser resize...
+  $(window).resize(function() {
+      moveProgressBar();
+  });
+
+  // SIGNATURE PROGRESS
+  function moveProgressBar() {
+    console.log("moveProgressBar");
+      var getPercent = ($('.progress-wrap-sv').data('progress-percent') / 100);
+      var getProgressWrapWidth = $('.progress-wrap-sv').width();
+      var progressTotal = getPercent * getProgressWrapWidth;
+      var animationLength = 2500;
+
+      // on page load, animate percentage bar to data percentage length
+      // .stop() used to prevent animation queueing
+      $('.progress-bar-sv').stop().animate({
+          left: progressTotal
+      }, animationLength);
+  }
+      </script>
   </body>
   </html>
